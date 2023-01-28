dc59a5473c79908d1956443f193d68cf93548e75
JordanU <42947912+UJordan@users.noreply.github.com>
2023-01-27 18:45:58 -0800

    Navbar Toggle

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,41 @@
-import { Container, Navbar, NavbarBrand, Collapse, NavbarToggler, Nav, NavItem, NavDropdown, NavLink } from 'reactstrap';
+import { Navbar, NavbarBrand, Collapse, NavbarToggler, Nav, NavItem } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
 
 const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
 
     return (
         <Navbar className='navbar navbar-expand-sm navbar-light bg-light sticky-top'>
             <NavbarBrand>
-                <h4 className='mt-2'> CASSIDY & JORDAN </h4>
+                <h4 className='mt-2 ms-2'> CASSIDY & JORDAN </h4>
             </NavbarBrand>
-            <Nav className='ms-auto' navbar>
-                <NavItem>
-                    <NavLink>
-                        <i />HOME
-                    </NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink>
-                        <i />INFO
-                    </NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink>
-                        <i />RESERVATION
-                    </NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink>
-                        <i />CONTACT
-                    </NavLink>
-                </NavItem>
-            </Nav>
+
+            <NavbarToggler onClick={() => setMenuOpen(!menuOpen)}/>
+            <Collapse isOpen={menuOpen} navbar>
+                <Nav className='ms-auto' navbar>
+                    <NavItem>
+                        <NavLink className='nav-link' to='/'>
+                            <i />HOME
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink className='nav-link' to='/info'>
+                            <i />INFO
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink className='nav-link' to='/reservation'>
+                            <i />RESERVATION
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink className='nav-link' to='/contact'>
+                            <i />CONTACT
+                        </NavLink>
+                    </NavItem>
+                </Nav>
+            </Collapse>
         </Navbar>
         
     );
